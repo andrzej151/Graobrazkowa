0f5647631c1c2b964e13da1123a081654cdb002c
Andrzej <andrzej.dabrowski94@gmail.com>
2020-01-16 23:33:18 +0100

    AD)B-003 - class Punkt

--- gra.js
+++ gra.js
@@ -1,24 +1,51 @@
-function Point(x,y,r){
-    this.x=x;
-    this.y=y;
-    this.r=r;
-    this.show=false;
+
+class Punkt {
+    constructor(x,y,r) {
+        this.x=x;
+        this.y=y;
+        this.r=r;
+        this.show=false;
+        
+        this.box=$('<div />').addClass("point")
+                        .appendTo($('#points')); 
+        
+    }
+    
+    find(x,y){
+        
+        
+        return Math.abs((this.x-x)*(this.x-x)+(this.y-y)*(this.y-y))<this.r;
+    }
+    
     
     
 };
 
-Point.prototype.find=function(x,y){
-    console.log("ok");
-    
-}
+
+const showPoint = function(p){
+        
+        
+        
+       $('<div />').addClass("check").css('top', "" + p.y + "px")
+            .css('left', "" + p.x + "px")
+                        .appendTo($('#imgDif'));   
+    }
+
+
+
+let points = Array();
 
 
 
 
 $(document).ready(function(){
-    let img1="/img2.png";
-    let img2="/img2.png";
+    //zmienne
+    const img1="img2.png";
+    const img2="img2.png";
+    const images=$('#imgDif');
+    
   
+    //dodanie obrazkow
     $('<img />').attr('src', "" + img1 + "")         
                         .width('100%').height('100%')
                         .appendTo($('#imgOk')); 
@@ -27,12 +54,39 @@ $(document).ready(function(){
                         .width('100%').height('100%')
                         .appendTo($('#imgDif')); 
     
+ 
+    
+    
+ 
+    // klikniecie
     $('#imgDif').click(function(e){
     var x = e.pageX - this.offsetLeft;
     var y = e.pageY - this.offsetTop;
-        $('<div />').addClass("check").css('top', "" + y + "px")
-            .css('left', "" + x + "px")
-                        .appendTo($('#imgDif')); 
+       
+        
+        points.forEach((p, i)=>{
+            console.log(p+" "+ p.find(x,y));
+            if(p.find(x,y))
+                {
+                    console.log("ten");
+                    showPoint(p);
+                    
+                }
+            else{
+                if(i>=points.length-1)
+                    {
+                        
+                        points.push(new Punkt(x,y,10));
+                       
+                    }
+            }
+        })
+        
+       
+        
+        
+        
+        
         
     });
 
