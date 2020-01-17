759ecd269aa3f9697a8d2f4af29f8022f61e8c50
Andrzej <andrzej.dabrowski94@gmail.com>
2020-01-17 01:55:19 +0100

    ADOB-004 - redy simple games

--- gra.js
+++ gra.js
@@ -1,97 +1,87 @@
-
-class Punkt {
-    constructor(x,y,r) {
-        this.x=x;
-        this.y=y;
-        this.r=r;
-        this.show=false;
-        
-        this.box=$('<div />').addClass("point")
-                        .appendTo($('#points')); 
-        
-    }
-    
-    find(x,y){
-        
-        
-        return Math.abs((this.x-x)*(this.x-x)+(this.y-y)*(this.y-y))<this.r;
-    }
-    
-    
-    
-};
-
-
-const showPoint = function(p){
-        
-        
-        
-       $('<div />').addClass("check").css('top', "" + p.y + "px")
-            .css('left', "" + p.x + "px")
-                        .appendTo($('#imgDif'));   
-    }
-
-
-
-let points = Array();
-
-
-
-
-$(document).ready(function(){
-    //zmienne
-    const img1="img2.png";
-    const img2="img2.png";
-    const images=$('#imgDif');
-    
-  
-    //dodanie obrazkow
-    $('<img />').attr('src', "" + img1 + "")         
-                        .width('100%').height('100%')
-                        .appendTo($('#imgOk')); 
-    
-    $('<img />').attr('src', "" + img2 + "")         
-                        .width('100%').height('100%')
-                        .appendTo($('#imgDif')); 
-    
- 
-    
-    
- 
-    // klikniecie
-    $('#imgDif').click(function(e){
-    var x = e.pageX - this.offsetLeft;
-    var y = e.pageY - this.offsetTop;
-       
-        
-        points.forEach((p, i)=>{
-            console.log(p+" "+ p.find(x,y));
-            if(p.find(x,y))
-                {
-                    console.log("ten");
-                    showPoint(p);
-                    
-                }
-            else{
-                if(i>=points.length-1)
-                    {
-                        
-                        points.push(new Punkt(x,y,10));
-                       
-                    }
-            }
-        })
-        
-       
-        
-        
-        
-        
-        
-    });
-
-});
-
-
-
-
+	//CONFIG SECTION
+		
+		//set the radius of circle in pixels
+		var circle_radius = 20;
+		//set circle color
+		var circle_color = "#FF0000";
+		
+		//set picture size in pixels
+		var picture_width = 250;
+		var picture_height = 381;
+		//set the coordinates [px,py,0] of points with differences (as many as You want)
+		var points_table = [
+			[21,  92,	0],
+			[132, 33,	0],
+			[157, 170,	0],
+			[232, 166,	0],
+			[26,  307,	0]
+			];
+		
+		//Texts:
+		var s_find_diff = "Znajdź różnice: ";
+		var s_congrat = "Gratulacje! Zagadka rozwiązana!<br />Kliknij tutaj, aby zagrać ponownie.";
+		
+		//END OF CONFIG SECTION
+		
+		var found = 0;
+		
+		function isPointInCircle(x, y, center_x, center_y, radius)
+		{
+			var dx = x - center_x, dy = y - center_y;
+			var distsq = dx * dx + dy * dy;
+			var rsq = radius * radius;
+			
+			return distsq < rsq;
+		}
+		
+		$(document).ready(function() {
+			
+			var ctx = $("#spot_diff_image")[0].getContext("2d");
+			$('#spot_diff_image').attr({width:picture_width,height:picture_height});
+			$('#spot_diff_image2').css({width:picture_width,height:picture_height});
+			$("#spot_diff_container").css("width", picture_width*2);
+			$("#spot_diff_message").html(s_find_diff+ (points_table.length-found));
+			
+			$("#spot_diff_message").click(function(){
+				if(found == points_table.length)
+				{
+					ctx.clearRect(0, 0, picture_width, picture_height);
+					found = 0;
+					$("#spot_diff_message").html(s_find_diff+ (points_table.length-found));
+					//restore table
+					for(var i=0; i<points_table.length; i++)
+					points_table[i][2] = 0;
+				}
+			});
+			
+			$("#spot_diff_image,#spot_diff_image2").click(function(e){ //Click on one image
+				var x = e.pageX-$(this).offset().left;	//Calculate click coordinates
+				var y = e.pageY-$(this).offset().top;
+				//check if click was near points in table
+				for(var i=0; i<points_table.length; i++)
+				{
+					if(isPointInCircle(x,y,points_table[i][0],points_table[i][1], circle_radius) && (points_table[i][2] == 0)) //Correct!
+					{
+						++found; //Increment points counter
+						
+						//draw a circle
+						ctx.beginPath();
+						ctx.strokeStyle=circle_color;
+						ctx.lineWidth=3;
+						ctx.arc(points_table[i][0], points_table[i][1], circle_radius, 0, Math.PI*2, true); 
+						ctx.closePath();
+						ctx.stroke();
+						
+						//mark point in table as found
+						points_table[i][2] = 1;
+						//Update message
+						$("#spot_diff_message").html(s_find_diff+ (points_table.length-found));
+						//If all points all done
+						if(found == points_table.length)
+						{
+							$("#spot_diff_message").html(s_congrat);
+						}
+					} //if(isPointInCircle(...
+				} //for(...)
+			}); //$("#image,#image2").click(...
+		});
