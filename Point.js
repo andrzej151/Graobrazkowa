8bf066317388a9fefdee58ba3f190192a3ddbce1
Andrzej <andrzej.dabrowski94@gmail.com>
2020-01-20 10:11:08 +0100

    ADOB-006 Game ready

---
+++ Point.js
@@ -0,0 +1,15 @@
+Point.all ={};
+
+function Point(x,y,r){
+    this.x=x;
+    this.y=y;
+    this.r=r;
+    this.show=false;
+    
+    
+};
+
+Point.prototype.find=function(x,y){
+    console.log("ok");
+    
+}
